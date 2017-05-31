aca003230b8a429315bbbf7369d6e08cb1721a08
StefanClausen <stefanclaussen@me.com>
2017-05-31 11:24:16 +0100

    Map view displays pins

--- On The Map/MapViewController.swift
+++ On The Map/MapViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
     
     var studentInformation: StudentInformation {
         let delegate = UIApplication.shared.delegate as! AppDelegate
@@ -29,5 +31,85 @@ class MapViewController: UIViewController {
                 print("Error getting studentLocation: \(error)")
             }
         }
+        
+        addMapAnnotations()
+    }
+    
+    func addMapAnnotations() {
+        let locations = hardCodedLocationData()
+        
+        var annotations = [MKPointAnnotation]()
+        
+        for dictionary in locations {
+            let lat = CLLocationDegrees(dictionary["latitude"] as! Double)
+            let long = CLLocationDegrees(dictionary["longitude"] as! Double)
+            
+            let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
+            
+            let first = dictionary["firstName"] as! String
+            let last = dictionary["lastName"] as! String
+            let mediaURL = dictionary["mediaURL"] as! String
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            annotation.title = "\(first) \(last)"
+            annotation.subtitle = mediaURL
+            
+            annotations.append(annotation)
+        }
+        
+        self.mapView.addAnnotations(annotations)
+    }
+    
+    // MARK: - MKMapViewDelegate
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinTintColor = .red
+            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
+    
+    
+    // MARK: - Sample Data
+    
+    func hardCodedLocationData() -> [[String : AnyObject]] {
+        return  [
+            [
+                "createdAt" : "2015-02-24T22:27:14.456Z" as AnyObject,
+                "firstName" : "Jessica" as AnyObject,
+                "lastName" : "Uelmen" as AnyObject,
+                "latitude" : 28.1461248 as AnyObject,
+                "longitude" : -82.75676799999999 as AnyObject,
+                "mapString" : "Tarpon Springs, FL" as AnyObject,
+                "mediaURL" : "www.linkedin.com/in/jessicauelmen/en" as AnyObject,
+                "objectId" : "kj18GEaWD8" as AnyObject,
+                "uniqueKey" : 872458750 as AnyObject,
+                "updatedAt" : "2015-03-09T22:07:09.593Z" as AnyObject
+            ], [
+                "createdAt" : "2015-02-24T22:35:30.639Z" as AnyObject,
+                "firstName" : "Gabrielle" as AnyObject,
+                "lastName" : "Miller-Messner" as AnyObject,
+                "latitude" : 35.1740471 as AnyObject,
+                "longitude" : -79.3922539 as AnyObject,
+                "mapString" : "Southern Pines, NC" as AnyObject,
+                "mediaURL" : "http://www.linkedin.com/pub/gabrielle-miller-messner/11/557/60/en" as AnyObject,
+                "objectId" : "8ZEuHF5uX8" as AnyObject,
+                "uniqueKey" : 2256298598 as AnyObject,
+                "updatedAt" : "2015-03-11T03:23:49.582Z" as AnyObject
+            ]
+        ]
     }
 }
